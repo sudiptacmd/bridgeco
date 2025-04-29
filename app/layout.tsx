b7bac7727ac45836d41e3bfe15769f2c886a1513
Sudipta Goswami <56980003+sudiptacmd@users.noreply.github.com>
2025-04-30 01:31:56 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "The Bridge Co. | Your Bridge to Growth",
   description: "Where brands meet results, and influencers drive impact.",
   manifest: "/manifest.json",
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
